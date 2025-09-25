592912453825ab80f05b524714985952e4ca1e69
Justin Adams <harmonic.chronicles@gmail.com>
2025-09-25 12:22:20 -0400

    Testing base path configurations.

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    base: 'https://q2computing.github.io/website/',
+    base: 'q2computing.github.io/website/',
     plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
