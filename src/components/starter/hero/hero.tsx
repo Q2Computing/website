b4853466c332d784884dfef9f113d713c6c95061
Justin Adams <harmonic.chronicles@gmail.com>
2025-09-25 17:55:04 -0400

    Changing the hero image.

--- src/components/starter/hero/hero.tsx
+++ src/components/starter/hero/hero.tsx
@@ -1,11 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import styles from "./hero.module.css";
 import ImgThunder from "../../../media/thunder.png?jsx";
+import ImgInterpolation from "../../../media/interpolation.jpg?jsx";
 
 export default component$(() => {
   return (
     <div class={["container", styles.hero]}>
-      <ImgThunder class={styles["hero-image"]} alt="Image thunder" />
+      <ImgInterpolation class={styles["hero-image"]} alt="Image thunder" />
       <h1>
         So <span class="highlight">fantastic</span>
         <br />
