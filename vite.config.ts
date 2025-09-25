621e051f5aae16c196d6be6834a82764d593e1d0
Justin Adams <harmonic.chronicles@gmail.com>
2025-09-25 12:15:00 -0400

    It seems like the vite.config.ts file is essential to the website's interactivity, but there is a break down with it operating through github pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    base: 'dist/website/',
+    base: '/website/',
     plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
